8dd6da2e90b7d57f4574874c03585461dbfbbef9
Britballs <britneygaming4@gmail.com>
2021-11-12 13:39:26 -0500

    - Created constants for Player and Map class.

---
+++ core/src/gameoff2021/game/utilities/Constants.java
@@ -0,0 +1,23 @@
+package gameoff2021.game.utilities;
+
+public class Constants {
+
+    public static class Player{
+
+        /* stringballnoob: I created 13 constants. Constants are values that remain the same.
+        * Constants cannot be modified/changed.
+        * "Final" word indicates constants cannot be changed.
+        */
+
+        public static final float MAX_SPEEDX = 15, MAX_SPEEDY = 15;
+        public static final float UNSCALED_WIDTH = 256, UNSCALED_HEIGHT = 256;
+        public static final float INIT_X = 825, INIT_Y= 390;
+        public static final float SCALE = .5f;
+        public static final float HITBOXOFFSET_X = 64, HITBOXOFFSET_Y = 64;
+    }
+
+    public static class Map{
+        public static final float MAP_WIDTH = 1920, MAP_HEIGHT = 1080;
+        public static final float X = 0, Y = 0;
+    }
+}
