42ddb3327ca711c0f1032a789b6317f9b5dab22b
Ghozti <ybalderas1005@gmail.com>
2021-11-17 16:02:46 -0500

    - filled in TileSet

--- core/src/gameoff2021/game/entities/map/TileSet.java
+++ core/src/gameoff2021/game/entities/map/TileSet.java
@@ -1,4 +1,42 @@
 package gameoff2021.game.entities.map;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.awt.*;
+
 public class TileSet {
+
+    float[] position;
+    float width, height;
+    Rectangle hitbox;
+
+    TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("textures/sprites.atlas"));// texture atlas instantiation and file path
+    TextureRegion debugTexture = atlas.findRegion("hitboxDebug");// hitbox texture region
+
+    public TileSet(float width, float height, float x, float y){
+        position = new float[]{0,0};
+        this.width = width;
+        this.height = height;
+        position[0] = x;
+        position[1] = y;
+
+        hitbox = new Rectangle();
+        hitbox.width = width;
+        hitbox.height = height;
+        hitbox.x = position[0];
+        hitbox.y = position[1];
+    }
+
+    public boolean isTouched(Rectangle hitbox){
+        return this.hitbox.overlaps(hitbox);
+    }
+
+    public void draw(Batch batch){
+        batch.draw(debugTexture, position[0],position[1],width,height);
+    }
+
 }
