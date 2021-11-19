862a055b1cafcbd35d10756b7cb89e01d8026364
Ghozti <ybalderas1005@gmail.com>
2021-11-18 20:55:27 -0500

    - update check

---
+++ core/assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="30" tileheight="30" tilecount="68" columns="34">
+ <image source="tiles.png" width="1024" height="64"/>
+</tileset>
