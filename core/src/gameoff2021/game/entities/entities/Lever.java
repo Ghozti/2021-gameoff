3d075a5d0ca758f30a9866e4a535739f5179f701
Britballs <britneygaming4@gmail.com>
2021-11-27 23:49:31 -0500

    - Created new Lever class. - Created right and left lever sprites.

---
+++ core/src/gameoff2021/game/entities/entities/Lever.java
@@ -0,0 +1,16 @@
+package gameoff2021.game.entities.entities;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import gameoff2021.game.entities.builder.GameSprite;
+
+public class Lever extends GameSprite {
+    @Override
+    public void draw(Batch batch) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
